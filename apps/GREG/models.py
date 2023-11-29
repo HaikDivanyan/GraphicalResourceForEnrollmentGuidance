ddfa65fbe8c175520c8fc13a48e9298dd9ff6883
Newsha-N <newshanz2000@gmail.com>
2023-11-28 17:32:12 -0800

    model and serializer

--- apps/GREG/models.py
+++ apps/GREG/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import int_list_validator
 
-# Create your models here.
+# models are not final and need to change depend on the clinet and scraping requirments
+# creating a schedule model 
+
+
+class ClassObj (models.Model):
+    uints = models.IntegerField()
+    subjectArea = models.CharField(max_length= 50)
+    rating = models.FloatField()
+    gradeDistribution = models.CharField(validators=[int_list_validator], max_length=100) # list of Int ? 
+    hotSeat = models.CharField(max_length=100)
+    classId = models.CharField(max_length=50) # double check the max length
+
+class Schedule (models.Model):
+    rating = models.FloatField() #could use ManytoManyField which creates schedule-class table 
+    classes = models.ArrayField(model_container=ClassObj)
+
+#how does this work? sample needed 
+class Time (models.Model):
+    days = models.CharField(max_length=20)
+    hours = models.CharField(max_length=20)
+    def __init__(self, days: str = None, hours: str = None):
+        self.days = days
+        self.hours = hours
+
+class UserFilters (models.Model):
+    priorityClasses = models.ArrayField(model_container=ClassObj)
+    ignoreClasses = models.ArrayField(model_container=ClassObj)
+    # user can choose multiple subject areas, shouldn't this be a list? 
+    # subjectArea = models(models.CharField(max_length=10, blank=True),size=8) 
+    subjectArea = models.CharField(max_length=100)
+    earliestStartTime = models.ManyToManyField(Time)
+    latestEndTime = models.ManyToManyField(Time)
+    preferreDays =  models.ManyToManyField(Time)
+    minClassRating = models.FloatField()
+    maxUnites = models.IntegerField(default=12)
+    numClasses = models.IntegerField(default=3)
+
