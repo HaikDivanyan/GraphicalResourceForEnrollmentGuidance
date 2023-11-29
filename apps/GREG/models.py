95c9cb3481a01598a3b725ecbdab6af67b1f689c
Newsha-N <newshanz2000@gmail.com>
2023-11-28 20:51:55 -0800

    api setup completed

--- apps/GREG/models.py
+++ apps/GREG/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import int_list_validator
+# from django.contrib.postgres.fields import ArrayField 
 
 # models are not final and need to change depend on the clinet and scraping requirments
 # creating a schedule model 
@@ -15,7 +16,10 @@ class ClassObj (models.Model):
 
 class Schedule (models.Model):
     rating = models.FloatField() #could use ManytoManyField which creates schedule-class table 
-    classes = models.ArrayField(model_container=ClassObj)
+    # classes = ArrayField(model_container=ClassObj)
+
+    def __str__(self):
+        return self.rating
 
 #how does this work? sample needed 
 class Time (models.Model):
@@ -25,16 +29,16 @@ class Time (models.Model):
         self.days = days
         self.hours = hours
 
-class UserFilters (models.Model):
-    priorityClasses = models.ArrayField(model_container=ClassObj)
-    ignoreClasses = models.ArrayField(model_container=ClassObj)
-    # user can choose multiple subject areas, shouldn't this be a list? 
-    # subjectArea = models(models.CharField(max_length=10, blank=True),size=8) 
-    subjectArea = models.CharField(max_length=100)
-    earliestStartTime = models.ManyToManyField(Time)
-    latestEndTime = models.ManyToManyField(Time)
-    preferreDays =  models.ManyToManyField(Time)
-    minClassRating = models.FloatField()
-    maxUnites = models.IntegerField(default=12)
-    numClasses = models.IntegerField(default=3)
+# class UserFilters (models.Model):
+#     # priorityClasses = ArrayField(model_container=ClassObj)
+#     # ignoreClasses = ArrayField(model_container=ClassObj)
+#     # user can choose multiple subject areas, shouldn't this be a list? 
+#     # subjectArea = models(models.CharField(max_length=10, blank=True),size=8) 
+#     subjectArea = models.CharField(max_length=100)
+#     earliestStartTime = models.ManyToManyField(Time)
+#     latestEndTime = models.ManyToManyField(Time)
+#     preferreDays =  models.ManyToManyField(Time)
+#     minClassRating = models.FloatField()
+#     maxUnites = models.IntegerField(default=12)
+#     numClasses = models.IntegerField(default=3)
 
