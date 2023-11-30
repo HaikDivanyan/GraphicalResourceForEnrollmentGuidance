3605a50fa63e9695808128ac25170b35511c3023
Newsha-N <newshanz2000@gmail.com>
2023-11-30 13:32:27 -0800

    server scrapes dars

--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from apps.GREG.views import greg_endpoint
+from apps.GREG.views import v
 from django.urls import path, include
 from apps.GREG import urls as api_urls
 
 urlpatterns = [
-    path('', greg_endpoint),
+    path('' , v),
     path('', include('rest_framework.urls')),
     path('', include(api_urls)),
 ]
