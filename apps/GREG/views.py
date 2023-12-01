676b48e9168b1a84cbcf78febded6e4eb0629df3
Newsha-N <newshanz2000@gmail.com>
2023-12-01 13:49:04 -0800

    clean up

--- apps/GREG/views.py
+++ apps/GREG/views.py
@@ -15,24 +15,26 @@ from .models import *
 from .scraping.DataController import DataController
 dataControler = DataController()
 
-
-def upload_file(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-          darspars(request.FILES["file"])
-        
-
 @api_view(['GET', 'POST'])
+def upload_file(request):
+        form = UploadFileForm(request.POST, request.FILES)
+        #if form.is_valid():
+          # darspars(request.FILES["file"])
+        dars = Dars.objects.all()
+        serializer = DarsSerializer(dars, many=True)
+        return Response(serializer.data)
+
 def darspars(f):
   with open("scraping test scripts/dar.html") as fil:
     dars = dataControler.parseDar(fil.read())
+  darsObj = Dars()
+  darsObj.save()
   for professor in dars.professors:
-    p = Professor(name= professor.name, rating = professor.rating)
+    p = Professor(name= professor.name, rating = professor.rating, dars= darsObj)
     p.save()
 
   for cls in dars.classes:
-    c = ClassObj(name= cls.name, units = cls.units, rating = cls.rating, subjectArea = cls.subjectArea, hotseatGraph= cls.hosteatGraph,gradeDistributions= cls.gradeDistributions, classId=cls.id)
+    c = ClassObj(name= cls.name, units = cls.units, rating = cls.rating, subjectArea = cls.subjectArea, hotseatGraph= cls.hosteatGraph,gradeDistributions= cls.gradeDistributions, classId=cls.id, dars= darsObj)
     c.save()
     for lect in cls.lectures:
       l = Lecture(classId = lect.id, classObj=c, professors= lect.professors)
@@ -48,20 +50,19 @@ def darspars(f):
             tmc.save()
 
   for req in dars.requirements:
-    r = Requirement(name = req.name)
+    r = Requirement(name = req.name, dars= darsObj)
     r.save()
     for sub in req.subrequirements:
        su = SubRequirement(name = sub.name, units = sub.units , count=sub.count, classes = sub.classes, subrequirements = r)
        su.save()
-
-  professors = Professor.objects.all()
-  profserializer = ProfessorSerializer(professors, many=True)
-  requirements = Requirement.objects.all()
-  reqSerializer = RequirementSerializer(requirements, many = True)
-  classes = ClassObj.objects.all()
-  clasSerializer = ClassSerializer(classes, many=True)
-  response = {'professors': profserializer.data , 'requirements': reqSerializer.data, 'classes': clasSerializer.data}
-
-  return Response(response)
+ 
+  # professors = Professor.objects.all()
+  # profserializer = ProfessorSerializer(professors, many=True)
+  # requirements = Requirement.objects.all()
+  # reqSerializer = RequirementSerializer(requirements, many = True)
+  # classes = ClassObj.objects.all()
+  # clasSerializer = ClassSerializer(classes, many=True)
+  # response = {'professors': profserializer.data , 'requirements': reqSerializer.data, 'classes': clasSerializer.data}
+  
 
     
