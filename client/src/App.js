1501b6c0b3e87e1d1a24bbfa4fd86b737a7cf41e
mop03 <36209272+mop03@users.noreply.github.com>
2023-12-02 17:16:52 -0800

    working except requirements display

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Landing from './components/landing';
 import NavBar from './components/navbar';
 import Instruction from './components/instruction';
@@ -8,6 +8,15 @@ import Footer from './components/footer';
 import './App.css';
 
 function App() {
+  const [remainingClasses, setClasses] = useState();
+  const [remainingProfessors, setProfessors ] = useState();
+  const [remainingRequirements, setRequirements] = useState();
+
+  useEffect(() => {
+    // This code will run every time remainingClasses is updated
+   // console.log('Remaining Classes:', remainingClasses);
+  }, [remainingClasses, remainingProfessors, remainingRequirements]);
+
   return(
     <div>
       <NavBar/>
@@ -15,9 +24,19 @@ function App() {
       <div className='BlueSection'></div>
       <Instruction/>
       <div className='BlueSection'></div>
-      <Upload/>
+      <Upload
+        setRemainingClasses={setClasses}
+        setRemainingProfessors={setProfessors}
+        setRemainingRequirements={setRequirements}
+      />
+      {console.log("HI")}
+      {console.log(remainingClasses)}
       <div className='BlueSection'></div>
-      <Remaining/>
+      <Remaining
+        sendRemainingClasses = {remainingClasses}
+        sendRemainingProfessors = {remainingProfessors}
+        sendRemainingRequirements = {remainingRequirements}
+      />
       <Footer/>
     </div>
   )
