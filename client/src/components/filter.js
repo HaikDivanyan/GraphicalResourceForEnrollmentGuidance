ce21b0fd44dc882962732f2af0eaf17f1b551c6b
mop03 <36209272+mop03@users.noreply.github.com>
2023-12-03 11:35:03 -0800

    sending back dars file and filters

--- client/src/components/filter.js
+++ client/src/components/filter.js
@@ -2,7 +2,7 @@ import './filter.css';
 import React, { useState } from 'react';
 import Select from 'react-select';
 
-export default function Filter({ sendRemainingClasses,  sendRemainingProfessors,  sendRemainingRequirements}) {
+export default function Filter({ sendRemainingClasses,  sendRemainingProfessors,  sendRemainingRequirements, sendFileBack}) {
 
   //sending this to backend
   const [filters, setFilters] = useState({
@@ -22,19 +22,24 @@ export default function Filter({ sendRemainingClasses,  sendRemainingProfessors,
     maxNumClasses: 5,
   });
 
+  
+
   const url = "http://127.0.0.1:8000/schedules/";
 
+  let formData = new FormData()
   //send back dars data
-  const darslist = [sendRemainingProfessors, sendRemainingRequirements, sendRemainingClasses];
+  if (sendFileBack) {
+  formData.append('file', sendFileBack[0])
+  }
+  
   //send to backend
-
   const requestBody = {
-    darslist: darslist,
+    darslist: formData,
     filters: filters
   }
   const handleSendFilters = () => {
     console.log('Filters:', filters);
-    console.log('Dars:', darslist);
+    console.log('Dars:', sendFileBack);
 
     fetch(url, {
       method: 'POST',
