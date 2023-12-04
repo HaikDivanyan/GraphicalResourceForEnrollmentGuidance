158f37f1d7ca0581494f2c29cade283713097c26
Newsha-N <newshanz2000@gmail.com>
2023-12-03 19:35:49 -0800

    shell script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Django
+python -m pip install django
+
+
+# Install additional packages
+pip install djangorestframework
+pip install django_cors_headers
+pip install BeautifulSoup4
+pip install selenium
+pip install numpy
+pip install webdriver_manager
