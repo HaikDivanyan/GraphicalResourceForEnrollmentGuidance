439f7bd1183576bf0ea98c9ccf88c2bfd37d2ae2
mop03 <36209272+mop03@users.noreply.github.com>
2023-12-03 19:32:09 -0800

    BruinWalk stuff

--- client/src/components/download.js
+++ client/src/components/download.js
@@ -1,9 +1,12 @@
 import './download.css';
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import html2canvas from 'html2canvas';
+import Chart from "chart.js/auto";
 
 export default function Download() {
   
+    const chartRef = useRef(null);
+
     const handleCaptureScreenshot = () => {
     const element = document.getElementById('capture'); // Replace 'capture' with the ID of the element you want to capture
 
@@ -25,6 +28,54 @@ export default function Download() {
     });
   };
 
+  useEffect(() => {
+    const gradeDistributions = {
+        "('SARRAFZADEH', '21F')": [5, 5, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "('SARRAFZADEH', '22W')": [1, 16, 4, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        "('SARRAFZADEH', '22S')": [4, 14, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        "('SARRAFZADEH', '22F')": [4, 5, 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "('SARRAFZADEH', '23S')": [7, 9, 3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    };
+        
+    
+    const categories = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
+    
+    const datasets = Object.entries(gradeDistributions).map(([key, values]) => ({
+        label: key,
+        data: values,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+    }));
+    
+     // Destroy the previous Chart instance if it exists
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    const ctx = document.getElementById('gradeHistogram').getContext('2d');
+    
+    chartRef.current = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: categories,
+        datasets: datasets
+        },
+        options: {
+        scales: {
+            x: {
+                type: 'category',
+                labels: categories
+            },
+            y: {
+                beginAtZero: true,
+                stepSize: 1
+            }
+        }
+        }
+    });
+    }, []);
+
   return (
     <div>
       <div className="UploadSection" id="capture">
@@ -36,6 +87,9 @@ export default function Download() {
       </div>
       <div className="ButtonContainer">
         <button className="generateButton" onClick={handleCaptureScreenshot}>Download Screenshot</button>
+      </div>
+      <div className="Canvas">
+      <canvas id="gradeHistogram" width="10" height="5"></canvas>
       </div>
     </div>
   );
