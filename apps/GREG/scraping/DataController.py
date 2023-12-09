bd203c466920415ea69ed91c3d89e7bb98fb9398
edanchu <91294716+edanchu@users.noreply.github.com>
2023-12-08 19:43:13 -0800

    bruinwalk, controller, and parser comments

--- apps/GREG/scraping/DataController.py
+++ apps/GREG/scraping/DataController.py
@@ -9,7 +9,13 @@ from pathlib import Path
 
 
 class DataController:
+    """
+    Main class that coordinates the parsing of Dars with the creation of ClassObjects, Requirement objects and Professor objects.
+    """
     def __init__(self):
+        """
+        Initializes a DataController object with instiated DarsParser, BruinwalkController, RegistrarController, and HotseatController objects along with loading the grade distribution cache.
+        """
         self.darsParser = DarsParser()
         self.BwController = BruinwalkController()
         self.regController = RegistrarController()
@@ -18,6 +24,11 @@ class DataController:
             self.distributions = pickle.load(f)
 
     def parseDar(self, dar: str) -> Dars:
+        """
+        Parses a Dar file and returns a Dars object.
+        :param dar: The html dar to parse
+        :return: Returns the built Dars object
+        """
         tempClasses, requirements = self.darsParser.parseDar(dar)
         classes = []
         professors = []
@@ -35,6 +46,11 @@ class DataController:
         return Dars(requirements, classes, professors)
 
     def _getClassInfo(self, cid) -> ClassObject:
+        """
+        A helper class to create and build a class object from the classid string. Concentrates data from hotseat, bruinwalk, registrar, and grad distribution cache.
+        :param cid: The class id of the class to get the data for
+        :return: Returns a class object
+        """
 
         currClass = ClassObject(id=cid)
         regData = self.regController.getClassData(currClass)
@@ -56,11 +72,21 @@ class DataController:
         return currClass
     
     def _getProfessorInfo(self, profName: str) -> Professor:
+        """
+        A helper class to create and build a professor object from the professor name string. Concentrates data from bruinwalk..
+        :param profName: The name of the professor to get the data for
+        :return: returns a professor object
+        """
         p = Professor(profName)
         p.rating = self.BwController.getProfessorRating(p)
         return p
     
     def _getGradeDistributions(self, currClass: ClassObject) -> dict[list[int]]:
+        """
+        A helper class to get the grade distribution for a class.
+        :param currClass: ClassObject for which to retrieve the grade distributions.
+        :return: Returns a dict of the form {(profname, quarter): grade distribution}
+        """
         if currClass.id not in self.distributions:
             return None
         else:
